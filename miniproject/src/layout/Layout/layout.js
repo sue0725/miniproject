16599beb8de1c55325cfdd1885eee3c76794a544
sue0725 <suhyunn0725@gmail.com>
2024-04-04 18:04:44 +0900

    changeg

--- miniproject/src/layout/Layout/layout.js
+++ miniproject/src/layout/Layout/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./layout.css"
-import Footer from "./footer";
-import Header from "./header";
+import Footer from "../Footer/footer";
+import Header from "../Header/header";
 
 
 const layout = () => {
